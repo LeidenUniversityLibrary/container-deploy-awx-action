0fa48910f6ebb824bf2f6d1bba2d73c9c9ed7bb5
nauwelaertse <nauwelaertse@vuw.leidenuniv.nl>
2025-11-03 16:50:54 +0100

    Delete boilerplate files

--- resources/views/errors/401.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('errors::illustrated-layout')
-
-@section('title', __('Unauthorized'))
-@section('code', '401')
-@section('message', __(''))
-
-@section('image')
-<div style="background-image: url('{{mix('img/ul_logo.png')}}'); background-size:contain" class="absolute pin bg-no-repeat md:bg-left lg:bg-center"></div>
-@endsection
--- resources/views/errors/500.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('errors::illustrated-layout')
-
-@section('title', __('Server Error'))
-@section('code', '500')
-@section('message', __(''))
-
-@section('image')
-<div style="background-image: url('{{mix('img/ul_logo.png')}}'); background-size:contain" class="absolute pin bg-no-repeat md:bg-left lg:bg-center"></div>
-@endsection
